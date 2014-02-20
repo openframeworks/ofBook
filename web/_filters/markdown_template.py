cd35fe680c054cb8b355d2d16206e40ecc0d135b
arturo <arturo@openframeworks.cc>
2014-02-20 17:48:38 +0100

    web: markdown_matjsx extension

--- web/_filters/markdown_template.py
+++ web/_filters/markdown_template.py
@@ -1,5 +1,9 @@
 import markdown
 import logging
+import sys
+import os
+sys.path.append(os.getcwd()+"/_filters")
+import mdx_mathjax
 
 config = {
     'name': "Markdown",
@@ -13,4 +17,4 @@ logging.getLogger("MARKDOWN").setLevel(logging.ERROR)
 
 
 def run(content):
-    return markdown.markdown(content, ['fenced_code','footnotes','headerid'])
+    return markdown.markdown(content, ['fenced_code','footnotes','headerid','mathjax'], output_format="html5")
