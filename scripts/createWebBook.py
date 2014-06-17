db710b0df22583dba6513f60f4e4553591a68a91
Michael Hadley <michaelwesthadley@gmail.com>
2014-06-17 07:40:42 -0500

    Updated pandoc options to better match shell script on master branch.  Table of contents was added with depth 4, tabs are preserved in fenced code, and math equation rendering was added via mathjax

--- scripts/createWebBook.py
+++ scripts/createWebBook.py
@@ -2,13 +2,10 @@ import os
 import subprocess
 import shutil
 
-#we will pase out HTML and change some tags, so we use beutiful soup: 
-#sudo pip install https://github.com/syabro/soupselect/archive/master.zip
-
+# Dependencies:
+# BeautifulSoup4: sudo pip install BeautifulSoup4
 
 from bs4 import BeautifulSoup as Soup
-from soupselect import select
-
 
 #todo: all the path stuff here is linux / osx friendly.  deal with windows....
 
@@ -52,7 +49,8 @@ for chapter in chapters:
 	source = "../chapters/" + chapter + "/chapter.md";
 	dest = path + "/index.html"
 
-	subprocess.call(["pandoc", "-o", dest, source, "-s", 
+	subprocess.call(["pandoc", "-o", dest, source, "-s", "--toc", "--toc-depth=4", "-p",
+					"--mathjax",	
 					"--include-in-header=createWebBookTemplate/IncludeInHeader.html",
 					"--include-before-body=createWebBookTemplate/IncludeBeforeBody.html",
 					"--include-after-body=createWebBookTemplate/IncludeAfterBody.html"])
@@ -66,12 +64,10 @@ for chapter in chapters:
 
 	if os.path.exists(dest):
 		soup = Soup(open(dest).read())
-		h1s = select(soup, 'h2')
-		pCaption = select(soup, "p.caption")
+		h1s = soup.find_all("h2")
+		pCaption = soup.find_all("p", class_="caption")
 		for tag in pCaption:
 			tag.name = "span"
-			#print tag
-			#print tag.name
 		html = soup.encode("utf-8")
 		with open(dest, "wb") as file:
 			file.write(html)
