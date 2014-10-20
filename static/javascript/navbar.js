4abc2912956db08c4a1cb044ea61c3596e2bbf6f
zach lieberman <zach@eyebeam.org>
2014-10-20 07:06:55 -0400

    css work

--- static/javascript/navbar.js
+++ static/javascript/navbar.js
@@ -6,6 +6,11 @@ $( document ).ready(function() {
     $(this).addClass('selected');
   })
 
+  $(".section").click(function () {
+    $('.section.selected').removeClass('selected');
+    $(this).addClass('selected');
+  })
+
 
   // Cache selectors
   var menu = $(".chapter.selected ul");
@@ -39,9 +44,9 @@ $( document ).ready(function() {
     cur = cur[cur.length-1];
     var id = cur && cur.length ? cur[0].id : "";
     // Set/remove active class
-    menuItems
-      .parent().removeClass("selected")
-      .end().filter("[href=#"+id+"]").parent().addClass("selected");
+    // menuItems
+    //   .parent().removeClass("selected")
+    //   .end().filter("[href=#"+id+"]").parent().addClass("selected");
   });
 
 });
