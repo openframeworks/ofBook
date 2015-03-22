292295b3eba26f1dc91881ccc57bf75cfd865d04
ruxrux <rux.pgp@gmail.com>
2015-03-22 12:57:51 -0400

    updating examples
    poly

--- chapters/OOPs!/examples/oops_6/src/ofBallBlue.h
+++ chapters/OOPs!/examples/oops_6/src/ofBallBlue.h
@@ -1,11 +1,3 @@
-//
-//  ofBallBlue.h
-//  oops! - object oriented programming
-//
-//  Created by rux for ofBook
-//
-//
-
 #pragma once				// another and more modern way to prevent the compiler from including thiis file more than once
 
 #include "ofMain.h"
