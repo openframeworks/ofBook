e178a326c4ffbd1b99719a7c86948b71809a8596
tpltnt <tpltnt.github@dropcut.net>
2015-05-20 11:04:03 +0200

    code scrubbing '14_sinExample_atan'

--- chapters/animation/code/14_sinExample_atan/src/rectangle.h
+++ chapters/animation/code/14_sinExample_atan/src/rectangle.h
@@ -6,24 +6,16 @@
 class rectangle {
 
 	public:
-	
+
         rectangle();
-	
+
 		void	draw();
 		void	xenoToPoint(float catchX, float catchY);
-	
+
 		ofPoint		pos;
 		ofPoint		prevPos;
-			
 		float		angle;
-	
-	
 		float		catchUpSpeed;		// take this pct of where I want to be, and 1-catchUpSpeed of my pos
-	
-
-	
-	
-	
 };
 
 #endif // RECTANGLE_H
--- chapters/animation/code/14_sinExample_atan/src/testApp.cpp
+++ chapters/animation/code/14_sinExample_atan/src/testApp.cpp
@@ -3,24 +3,17 @@
 
 //--------------------------------------------------------------
 void testApp::setup(){
-
-	
 	// macs by default run on non vertical sync, which can make animation very, very fast
 	// this fixes that: 
-	
 	ofSetVerticalSync(true);
-	
+
 	// set background: 
-	
 	ofEnableAlphaBlending();
 	ofBackground(30,30,30);
-	
-	
+
 	// set the position of the rectangle:
-	
 	myRectangle.pos.x = 100;
 	myRectangle.pos.y = 50;
-	
 }
 
 //--------------------------------------------------------------
