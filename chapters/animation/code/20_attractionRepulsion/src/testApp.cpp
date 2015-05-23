acca906b94fc9edcb93fd02a17ee495f686118f8
tpltnt <tpltnt.github@dropcut.net>
2015-05-23 10:07:20 +0200

    code scrubbing '20_attractionRepulsion'

--- chapters/animation/code/20_attractionRepulsion/src/testApp.cpp
+++ chapters/animation/code/20_attractionRepulsion/src/testApp.cpp
@@ -2,12 +2,10 @@
 
 
 //--------------------------------------------------------------
-void testApp::setup(){	
-	
+void testApp::setup(){
 	ofSetVerticalSync(true);
 	ofSetFrameRate(60);
-	
-	
+
 	for (int i = 0; i < 1000; i++){
 		particle myParticle;
 		myParticle.setInitialCondition(ofRandom(0,1000),ofRandom(0,1000),0,0);
@@ -16,30 +14,25 @@ void testApp::setup(){
 		//myParticle.damping = ofRandom(0.01, 0.05);
 		particles.push_back(myParticle);
 	}
-	
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
 
-	// on every frame 
+    // on every frame 
 	// we reset the forces
 	// add in any forces on the particle
 	// perfom damping and
 	// then update
-	
-	
+
 	for (int i = 0; i < particles.size(); i++){
 		particles[i].resetForce();
-		
-		
 		//particles[i].addAttractionForce(mouseX, mouseY, 1000, 0.1);
 		//particles[i].addRepulsionForce(mouseX, mouseY, 60, 1);
-		
-		
+
 		particles[i].addCounterClockwiseForce(mouseX, mouseY, 1000, 0.1);
 		particles[i].addClockwiseForce(mouseX, mouseY, 200, 1);
-		
+
 		//particles[i].addForce(0,0.04);  // gravity
 		particles[i].addDampingForce();
 		particles[i].update();
@@ -50,8 +43,8 @@ void testApp::update(){
 //--------------------------------------------------------------
 void testApp::draw(){
 
-	ofSetColor(0x000000);
-	
+	ofSetColor(0);
+
 	for (int i = 0; i < particles.size(); i++){
 		particles[i].draw();
 	}
