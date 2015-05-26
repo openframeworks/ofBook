1dfd98530b94167c47142310df7320082abe2e7f
tpltnt <tpltnt.github@dropcut.net>
2015-05-26 20:41:59 +0200

    code scrubbing '26_multiForces2'

--- chapters/animation/code/26_multiForces2/src/testApp.cpp
+++ chapters/animation/code/26_multiForces2/src/testApp.cpp
@@ -3,67 +3,53 @@
 
 //--------------------------------------------------------------
 void testApp::setup(){	
-	
-	ofBackground(0,0,0);
-	
-	ofSetVerticalSync(true);
-	ofSetFrameRate(60);
-	
-	
-	for (int i = 0; i < 100; i++){
-		particle myParticle;
-		
-		float x = 500 + 100 * cos ( (i / 100.0) * TWO_PI);
-		float y = 500 + 100 * sin ( (i / 100.0) * TWO_PI);
-		
-		myParticle.setInitialCondition(x,y ,0,0);
-		particles.push_back(myParticle);
+    ofBackground(0,0,0);
+    ofSetVerticalSync(true);
+    ofSetFrameRate(60);
+
+    for (int i = 0; i < 100; i++){
+        particle myParticle;
+        float x = 500 + 100 * cos ( (i / 100.0) * TWO_PI);
+        float y = 500 + 100 * sin ( (i / 100.0) * TWO_PI);
+        myParticle.setInitialCondition(x,y ,0,0);
+        particles.push_back(myParticle);
 	}
-	
-	// change this to
-	// for (int i = 0; i < 10; i++){
-	// to see a chain
-	
-	for (int i = 0; i < particles.size(); i++){
-		spring mySpring;
-		mySpring.distance		= 10;
-		mySpring.springiness	= 0.2f;
-		mySpring.particleA = & (particles[i  ]);
-		mySpring.particleB = & (particles[(i+1) % particles.size()]);
-		springs.push_back(mySpring);
+
+    // change this to
+    // for (int i = 0; i < 10; i++){
+    // to see a chain
+    for (int i = 0; i < particles.size(); i++){
+        spring mySpring;
+        mySpring.distance = 10;
+        mySpring.springiness = 0.2f;
+        mySpring.particleA = & (particles[i  ]);
+        mySpring.particleB = & (particles[(i+1) % particles.size()]);
+        springs.push_back(mySpring);
 	}
-	
-		
-	
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-
-	// on every frame 
-	// we reset the forces
-	// add in any forces on the particle
-	// perfom damping and
-	// then update
-	
-	for (int i = 0; i < particles.size(); i++){
-		particles[i].resetForce();
+    // on every frame 
+    // we reset the forces
+    // add in any forces on the particle
+    // perfom damping and
+    // then update
+    for (int i = 0; i < particles.size(); i++){
+        particles[i].resetForce();
 	}
-	
-	for (int i = 0; i < particles.size(); i++){
-		
+
+    for (int i = 0; i < particles.size(); i++){
 		particles[i].addRepulsionForce(mouseX, mouseY, 200, 0.3f);
-		
 		for (int j = 0; j < i; j++){
 			particles[i].addRepulsionForce(particles[j], 20, 0.3);
 		}
 	}
-	
+
 	for (int i = 0; i < springs.size(); i++){
 		springs[i].update();
 	}
-	
-	
+
 	for (int i = 0; i < particles.size(); i++){
 		particles[i].bounceOffWalls();
 		particles[i].addDampingForce();
@@ -71,10 +57,9 @@ void testApp::update(){
 	}
 }
 
+
 //--------------------------------------------------------------
 void testApp::draw(){
-	
-
 	ofSetColor(0xffffff);
 	
 	for (int i = 0; i < particles.size(); i++){
@@ -84,14 +69,12 @@ void testApp::draw(){
 	for (int i = 0; i < springs.size(); i++){
 		springs[i].draw();
 	}
-
 }
 
+
 //--------------------------------------------------------------
-void testApp::keyPressed  (int key){ 
-	
+void testApp::keyPressed(int key){ 
 	switch (key){
-			
 		case ' ':
 			// reposition everything: 
 			for (int i = 0; i < particles.size(); i++){
@@ -99,17 +82,18 @@ void testApp::keyPressed  (int key){
 			}
 			break;
 	}
-	
-	
 }
 
+
 //--------------------------------------------------------------
-void testApp::keyReleased  (int key){ 
+void testApp::keyReleased(int key){ 
 }
+
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ){
 }
+
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button){
@@ -120,14 +104,14 @@ void testApp::mouseDragged(int x, int y, int button){
 	particles.push_back(myParticle);*/
 }
 
+
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
 	particles[0].bFixed = true;
 }
 
+
 //--------------------------------------------------------------
 void testApp::mouseReleased(){
-	
 	particles[0].bFixed = false;
 }
-
