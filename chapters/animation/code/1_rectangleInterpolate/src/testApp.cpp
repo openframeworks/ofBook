e035ecfbdfc25a5ea67d9d8c65521c6d53443a2f
tpltnt <tpltnt.github@dropcut.net>
2015-06-13 18:02:14 +0200

    removed empty functions from '1_rectangleInterpolate'

--- chapters/animation/code/1_rectangleInterpolate/src/testApp.cpp
+++ chapters/animation/code/1_rectangleInterpolate/src/testApp.cpp
@@ -37,27 +37,7 @@ void testApp::draw(){
 }
 
 //--------------------------------------------------------------
-void testApp::keyPressed  (int key){
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased  (int key){
-}
-
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-}
-
-//--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
 	//myRectangle.posa.x = x;
 	//myRectangle.posa.y = y;	
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(){
 }
