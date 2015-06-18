660d210388c042dcd9b651fd4f63fdecb917d8e6
tpltnt <tpltnt.github@dropcut.net>
2015-06-18 13:42:55 +0200

    removed empty functions from '16_noise'

--- chapters/animation/code/16_noise/src/testApp.cpp
+++ chapters/animation/code/16_noise/src/testApp.cpp
@@ -6,10 +6,6 @@ void testApp::setup(){
 }
 
 //--------------------------------------------------------------
-void testApp::update(){
-}
-
-//--------------------------------------------------------------
 void testApp::draw(){
 	ofSetColor(255,0,127);
 
@@ -23,38 +19,3 @@ void testApp::draw(){
 	ofEndShape();
 }
 
-//--------------------------------------------------------------
-void testApp::keyPressed(int key){
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
-}
-
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-}
-
-//--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-}
-
-//--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
-}
