894a476f00535a70a2bd0390a069daf9bc733336
tpltnt <tpltnt.github@dropcut.net>
2015-06-18 14:00:14 +0200

    some code scrubbing for '17_noise'

--- chapters/animation/code/17_noise/src/testApp.cpp
+++ chapters/animation/code/17_noise/src/testApp.cpp
@@ -12,25 +12,22 @@ void testApp::setup(){
 void testApp::draw(){
 	ofEnableAlphaBlending();
 	ofSetColor(255,255,255, 100);
-	//colorScheme.draw(0,0, ofGetWidth(), ofGetHeight());
 
 	ofSeedRandom(0); // always pick the same random numbers.
 
 	for (int i = 0; i < mouseX*3; i++){
 		float x = ofNoise(ofGetElapsedTimef()*0.2, i*0.3)*ofGetWidth();
 		float y = ofNoise(-ofGetElapsedTimef()*0.2, i*0.3)*ofGetHeight();
-
 		ofColor col = getColorForPixel( ofMap(x,0,ofGetWidth(), 0, colorScheme.width),
-									   ofMap(y,0,ofGetHeight(), 0, colorScheme.height));
-
+						ofMap(y,0,ofGetHeight(), 0, colorScheme.height));
 		col.a = 85;	// set some alpha. 
 		ofSetColor(col);
 		ofCircle(x,y,ofRandom(4,40));
 	}
 }
 
+//--------------------------------------------------------------
 ofColor testApp::getColorForPixel(int x, int y){
-
 	if (x < 0) x = 0;
 	if (y < 0) y = 0;
 	if (x >= colorScheme.width) x = colorScheme.width-1;
