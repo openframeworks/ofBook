8123508c5897ae7fb4d2548e1da861de6ee8166b
tpltnt <tpltnt.github@dropcut.net>
2015-06-18 12:18:47 +0200

    removed empty functions from '12_sinExample_circlePlusPath'

--- chapters/animation/code/12_sinExample_circlePlusPath/src/testApp.cpp
+++ chapters/animation/code/12_sinExample_circlePlusPath/src/testApp.cpp
@@ -44,31 +44,3 @@ void testApp::draw(){
 	}
 	ofEndShape();
 }
-
-//--------------------------------------------------------------
-void testApp::keyPressed(int key){
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
-}
-
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-}
--- chapters/animation/code/12_sinExample_circlePlusPath/src/testApp.h
+++ chapters/animation/code/12_sinExample_circlePlusPath/src/testApp.h
@@ -10,14 +10,6 @@ class testApp : public ofBaseApp{
 		void update();
 		void draw();
 
-		void keyPressed  (int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-
 		vector <ofPoint> points;
 		float radius;
 };
