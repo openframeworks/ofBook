f70288bb0ae7a2af5fb7730545538a13f52e8d53
tpltnt <tpltnt.github@dropcut.net>
2015-06-18 12:03:37 +0200

    removed empty functions from '7_sinExample_phase'

--- chapters/animation/code/7_sinExample_phase/src/testApp.cpp
+++ chapters/animation/code/7_sinExample_phase/src/testApp.cpp
@@ -8,12 +8,8 @@ void testApp::setup(){
 }
 
 //--------------------------------------------------------------
-void testApp::update(){
-}
-
-//--------------------------------------------------------------
 void testApp::draw(){
-	float sinOfTime				= sin( ofGetElapsedTimef() );
+        float sinOfTime				= sin( ofGetElapsedTimef() );
 	float sinOfTimeMapped		= ofMap(sinOfTime, -1, 1, 0, 255);
 	
 	ofBackground(sinOfTimeMapped, sinOfTimeMapped, sinOfTimeMapped);
@@ -23,32 +19,4 @@ void testApp::draw(){
 	
 	ofSetColor(sinOfTimeMapped2, sinOfTimeMapped2, sinOfTimeMapped2);
 	ofRect(100,100,ofGetWidth()-200, ofGetHeight()-200);
-}
-
-//--------------------------------------------------------------
-void testApp::keyPressed(int key){
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
-}
-
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
 }
