b8cc2f4a26bbfbc890de5a7a95d6620fbf8f46b9
Arturo Castro <arturo@openframeworks.cc>
2016-01-21 16:57:25 +0000

    install_linux script

--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -1,3 +1,9 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sudo apt-get install python-pip python2.7-dev git pandoc ruby-sass texlive
-pip install beauitfulsoup4 libsass
-
+sudo pip install virtualenv
+cd ${DIR}/..
+virtualenv pythonenv -p /usr/bin/python2.7
+source pythonenv/bin/activate
+pip install beautifulsoup4
+pip install libsass
+deactivate
