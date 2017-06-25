ef363e110aca7d974e02a0cbf8492fdde282ce3e
Michael Hadley <michaelwesthadley@gmail.com>
2017-06-25 14:19:06 -0500

    Extend gulp: autoprefix sass, sourcemap & copy static files

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,50 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
+const autoprefixer  = require("gulp-autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const plumber = require("gulp-plumber");
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task("serve", function() {
     browserSync.init({
         server: "./output/webBook",
         directory: true,
         startPath: "/chapters"
     });
-    gulp.watch("static/style/*.scss", ['sass']);
-    gulp.watch("output/webBook/**").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("static/style/style.scss")
-        .pipe(sass())
+gulp.task("sass", function() {
+    return gulp.src("static/style/**/*.+(scss|sass)")
+        .pipe(plumber()) // Don't crash on error
+        .pipe(sourcemaps.init())
+            .pipe(sass({outputStyle: "compressed"}))
+            .pipe(autoprefixer({
+                browsers: ["last 2 versions"],
+                cascade: true
+            }))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest("output/webBook/style/"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['serve']);
+// Copy static files other than CSS
+gulp.task("copy", function() {
+    return gulp.src(
+            ["static/vendor/**/*.*", "static/javascript/**/*.*", "static/images/**/*.*"], 
+            {base: "static"}
+        )
+        .pipe(gulp.dest("output/webBook/"))
+});
+
+gulp.task("watch", function () {
+    gulp.watch("static/style/**/*", ["sass"]);
+    gulp.watch(
+        ["static/vendor/**/*.*", "static/javascript/**/*.*", "static/images/**/*.*"], 
+        ["copy"]
+    );
+    gulp.watch("output/webBook/**").on("change", browserSync.reload);
+});
+
+gulp.task("default", ["serve", "sass", "copy", "watch"]);
